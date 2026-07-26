d32d0b76723be2409a6cb89a362d1f1107d8d435
taniazubair <tzak2004@gmail.com>
2026-07-26 18:47:08 +0500

    Update SMTP configuration

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -17,9 +17,10 @@ router.post("/login", loginUser);
 
 // ====== FORGOT PASSWORD & RESET PASSWORD ROUTES (Yaha add kiye) ======
 
-// Nodemailer transporter setup
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
