a43825ceb38247ff5fb3650fed746323f0fbcb27
taniazubair <tzak2004@gmail.com>
2026-07-29 01:07:19 +0500

    Build complete SmartSpend landing page UI

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -5,7 +5,6 @@ import Hero from "../components/Hero";
 import Features from "../components/Features";
 import WhyChooseUs from "../components/WhyChooseUs";
 import HowItWorks from "../components/HowItWorks";
-import DashboardPreview from "../components/DashboardPreview";
 import CTA from "../components/CTA";
 import Footer from "../components/Footer";
 
@@ -17,7 +16,6 @@ function LandingPage() {
       <Features />
       <WhyChooseUs />
       <HowItWorks />
-      <DashboardPreview />
       <CTA />
       <Footer />
     </div>
